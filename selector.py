56d20253e9b5a4d01d0480f1e67b3e42f59d22b3
jdieguez1 <jdieguez@fi.uba.ar>
2024-01-05 20:34:17 -0300

    Guardar archivos

--- selector.py
+++ selector.py
@@ -1,5 +1,6 @@
 import os
-from tkinter import Tk, Button, Canvas, Frame, Label
+import shutil
+from tkinter import Scrollbar, Tk, Button, Canvas, Frame, Label, Toplevel
 from tkinter import filedialog
 from PIL import Image, ImageTk
 
@@ -22,7 +23,11 @@ class ImageViewer:
         
         self.load_button = Button(self.top_frame, font=font, text="Cargar carpeta", command=self.load_directory,
                                   width=20)
-        self.load_button.pack(side="top")
+        self.load_button.pack(side="left")
+
+        self.end_button = Button(self.top_frame, font=font, text="Finalizar selección", command=self.show_selected_images,
+                                  width=20)
+        self.end_button.pack(side="right")
 
         self.center_frame = Frame(self.root)
         self.center_frame.pack(fill="both", expand=True)
@@ -59,8 +64,6 @@ class ImageViewer:
         if self.images:
             image_path = self.images[self.current_image]
             image = Image.open(image_path)
-
-            print(self.selected_images)
 
             if self.current_image not in self.selected_images:
                 self.select_button.config(state="active")
@@ -100,6 +103,75 @@ class ImageViewer:
         if self.current_image not in self.selected_images:
             self.selected_images.append(self.current_image)
             self.select_button.config(state="disabled")
+
+    def save_selected_images(self, directory):
+        # Verificar si el directorio existe, si no, crearlo
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Copiar las imágenes seleccionadas al directorio
+        for i, image_index in enumerate(self.selected_images):
+            image_path = self.images[image_index]
+            shutil.copy(image_path, directory)
+
+    def show_selected_images(self):
+        selected_window = Toplevel(self.root)
+        selected_window.title("Imágenes seleccionadas")
+
+        selected_window.geometry("{0}x{1}".format(self.width, self.height))
+        selected_window.resizable(width=False, height=False)
+
+        img_width = 100  # Tamaño deseado de las miniaturas
+        img_height = 100
+
+        cols = max(1, self.width // img_width)
+        rows = max(1, len(self.selected_images) // cols)
+
+        canvas = Canvas(selected_window)
+        canvas.grid(row=0, column=0, sticky="nsew")
+
+        scrollbar = Scrollbar(selected_window, orient="vertical", command=canvas.yview)
+        scrollbar.grid(row=0, column=1, sticky="ns")
+
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        frame = Frame(canvas)
+        canvas.create_window((0, 0), window=frame, anchor="nw")
+
+        for i, image_index in enumerate(self.selected_images):
+            row_pos = i // cols
+            col_pos = i % cols
+
+            image_path = self.images[image_index]
+            image = Image.open(image_path)
+            image.thumbnail((img_width, img_height), Image.ANTIALIAS)
+            tk_image = ImageTk.PhotoImage(image)
+
+            label = Label(frame, image=tk_image)
+            label.grid(row=row_pos, column=col_pos, padx=5, pady=5)
+
+            label.image = tk_image
+
+        def on_configure(event):
+            canvas.configure(scrollregion=canvas.bbox("all"))
+
+        frame.bind("<Configure>", on_configure)
+
+        def confirm_button_clicked():
+            directory = filedialog.askdirectory(title="Seleccionar directorio para guardar imágenes")
+            os.makedirs(os.path.join(directory, "seleccion"), exist_ok=True)
+
+            if directory:
+                self.save_selected_images(directory + '/seleccion')
+                selected_window.destroy()
+                self.root.destroy()
+
+        confirm_button = Button(selected_window, font=("Helvetica", 16, "bold"), text="Confirmar", command=confirm_button_clicked)
+        confirm_button.grid(row=0, column=2, pady=10, padx=5, sticky="e")
+
+        selected_window.columnconfigure(0, weight=1)
+        selected_window.rowconfigure(0, weight=1)
+        selected_window.rowconfigure(1, weight=0)
 
 if __name__ == "__main__":
     root = Tk()
